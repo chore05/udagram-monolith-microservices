0fa18dda835f6b67bac571ea01d3423952ce3a2a
chore05 <chorevmichal@gmail.com>
2023-04-27 09:39:52 +0100

    project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=chore05
+export POSTGRES_PASSWORD=chore1234
+export POSTGRES_HOST=testdatabase.cy5zy4illlo6.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::test-209862822072-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
